07314d911bf45f68ce5dc92fe332a8b6c52e9aa1
James Middleton <hello@jamesmiddleton.me>
2020-07-15 17:33:01 +0100

    chore(deps): Add tailwind-vue

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './plugins'
+
 import Vue from 'vue'
 import App from './App.vue'
 
---
+++ src/plugins/tailwind.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+const VueTailwind = require('vue-tailwind')
+
+Vue.use(VueTailwind)
