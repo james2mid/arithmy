2a27f095774871db1a4b4f85ef689b30ad0d1fbc
James Middleton <hello@jamesmiddleton.me>
2020-07-15 17:16:19 +0100

    chore: Init and clean-up vue app

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    Root components go here
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {}
+</script>
