237e6aef734e8a64931fb0cdbd347f4dcee2f64b
James Middleton <hello@jamesmiddleton.me>
2020-07-23 13:23:44 +0100

    fix: Properly configure PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    'vue-cli-plugin-tailwind/purgecss': {},
+    'vue-cli-plugin-tailwind/purgecss': {
+      content: [
+        './node_modules/vue-tailwind/src/themes/default/*.js',
+        './src/**/*.vue',
+        './src/**/*.ts',
+        './public/index.html'
+      ]
+    },
     autoprefixer: {}
   }
 }
